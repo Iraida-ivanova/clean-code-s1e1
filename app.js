a56e54363a72066cdbc03fea6b2447dfa37cbef8
Iraida22 <iraida.iva22@mail.ru>
2022-05-24 15:28:24 +0300

    refactor: reduce the use of selector tags to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -33,17 +33,20 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
+    label.classList.add("task", "task-save");
+    listItem.className = "task-wrapper";
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className = "todo-checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.classList.add("task", "task-edit");
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="btn-edit";
+    editButton.classList.add("button-base", "btn-edit");
 
-    deleteButton.className="btn-delete";
+    deleteButton.classList.add("button-base", "btn-delete");
+    deleteButtonImg.className = "btn-del-img";
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
 
@@ -81,8 +84,7 @@ var editTask=function(){
 
 
     var listItem=this.parentNode;
-
-    var editInput=listItem.querySelector('input[type=text]');
+    var editInput=listItem.querySelector(".task-edit");
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".btn-edit");
     var containsClass=listItem.classList.contains("edit-mode");
